e3123a0a717a2ac9b46b9b6b701e011d092a9d22
udokachi <chidinma242@gmail.com>
2023-05-08 14:13:39 +0100

    refactored the get all country currency api

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -67,31 +67,35 @@ export const getCountryCapital = async (req, res) => {
 //  get each country's currency
 export const getCountryCurrency = async (req, res) => {
 
+    const { sCountryISOCode } = req.body; // Call the country currency SOAP API using axios 
     try {
-        const { sCountryISOCode } = req.body
-        const headers = {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
-        const response = await apiClient.post('/CountryCurrency', { sCountryISOCode }, headers)
-
-
-        let data = {
-            capital: response.data
-        };
-
-        return res.status(200).json({
-            message: 'Country Currency Retrived Successfully',
-            data
+        const response = await axios.post(
+            'http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso',
+            ` 
+                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.oorsprong.org/websamples.countryinfo"> 
+                    <soapenv:Header/> 
+                    <soapenv:Body> 
+                        <web:CountryCurrency> 
+                            <web:sCountryISOCode>${sCountryISOCode}</web:sCountryISOCode> 
+                        </web:CountryCurrency> 
+                    </soapenv:Body> 
+                </soapenv:Envelope> 
+            `, {
+            headers: { 'Content-Type': 'text/xml' }
+        }
+        ); // Extract the country currency from the SOAP response 
+        const xmlResponse = response.data;
+        parseString(xmlResponse, function (err, result) {
+            if (err) {
+                console.error(err); res.status(500).json({ error: 'Internal server error' });
+            } else {
+                const capital = result['soap:Envelope']['soap:Body'][0]['m:CountryCurrencyResponse'][0]['m:CountryCurrencyResult'][0];
+                res.status(200).json({ capital });
+            }
         });
-
-
     } catch (error) {
-        console.error(error)
-        res.status(404).json({
-            Error: 'Country not found'
-        })
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 }
 
